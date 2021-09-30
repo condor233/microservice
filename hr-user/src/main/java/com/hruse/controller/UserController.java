dd8303f8f1ae3b56dc563939983fce92711befcf
condor233 <e.hdelemos@gmail.com>
2021-09-30 10:53:36 -0300

    UserFeignClient

--- hr-user/src/main/java/com/hruse/controller/UserController.java
+++ hr-user/src/main/java/com/hruse/controller/UserController.java
@@ -31,7 +31,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/search")
-	public ResponseEntity<User> findById(@RequestParam String email) {		
+	public ResponseEntity<User> findByEmail(@RequestParam String email) {		
 		User emailUser = userRepository.findByEmail(email);
 		return ResponseEntity.ok(emailUser);	
 	}
